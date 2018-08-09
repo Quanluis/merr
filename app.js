60ca55825d654e37147351bc8b3524db11252017
Quanluis <quanluis@protonmail.com>
2018-08-09 16:14:36 -0400

    created a click event that hides elements on a page and displays others accordingly.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+$("#pic").hide()
+$("#letter").hide()
+
 $('#quote').hide().fadeIn(1500);
 
 $('#mindy').hide().slideDown(1500);
+
+$('#mindy').click(function(){
+    console.log("clicked")
+    $('#pic').show()
+    $('#letter').show()
+    $('#quote').hide()
+    $('#mindy').hide()
+
+})
